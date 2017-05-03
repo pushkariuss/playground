f4bbec2a27e5cd54466e245e2d8a2ff4c0ca9757
pikvall <zvalm@yandex.ru>
2017-05-03 12:15:00 +0300

    [valya] initial commit

---
+++ valya/src/com/valya/figures/Rectangle.java
@@ -0,0 +1,25 @@
+package com.valya.figures;
+
+    /**
+     * Created by vdmitrieva on 02.05.2017.
+     */
+    public class Rectangle extends Figure {
+        Rectangle(double a, double b) {
+            super(a, b);
+        }
+
+        public double square() {
+            return dim1 * dim2;
+        }
+
+        public double perimeter() {
+            return dim1 * dim2 * 2;
+        }
+
+        public String toString() {
+            return "Rectangle [ A = " + dim1 + " , B = " + dim2 + "] [ P = " + perimeter() + " ] [ S = " + square() + "]";
+
+        }
+
+
+    }
---
+++ valya/src/com/valya/modificators/Modifacators.java
@@ -0,0 +1,11 @@
+package com.valya.modificators;
+
+/**
+ * Created by vdmitrieva on 03.05.2017.
+ */
+public class Modifacators {
+    public int MyData;
+    public Modifacators() {
+        MyData = 123;
+    }
+}
