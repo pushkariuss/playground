83d348ad1d2b52f488ac380cdbf257e71b2846f3
pushkariuss <pushkariuss@gmail.com>
2017-05-04 11:00:14 +0300

    [jmh] some tests

---
+++ skovorodka/src/main/java/com/lolka/puzzlers/JmhTest.java
@@ -0,0 +1,32 @@
+package com.lolka.puzzlers;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by pushkariuss on 21.07.2016.
+ */
+@State(Scope.Thread)
+public class JmhTest {
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(JmhTest.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public  int sd() {
+        return 1;
+    }
+}
